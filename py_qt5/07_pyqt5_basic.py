eed3299093a6699910c036e25f4195236fe353a2
bluehyena <42088479+bluehyena@users.noreply.github.com>
2020-10-31 21:44:10 +0900

    Update 07_pyqt5_basic.py

--- py_qt5/07_pyqt5_basic.py
+++ py_qt5/07_pyqt5_basic.py
@@ -1,9 +1,8 @@
-# 메뉴바 만들기 https://wikidocs.net/21866
+# 툴바 만들기 https://wikidocs.net/21932
 
 """
-한 개의 메뉴를 갖는 메뉴바를 만들었습니다.
-이 메뉴는 클릭했을 때 어플리케이션을 종료하는 기능을 갖고 있습니다. 또한 이 기능은 단축키 (Ctrl+Q)로도 실행이 가능합니다.
-
+메뉴(menu)가 어플리케이션에서 사용되는 모든 명령의 모음이라면
+툴바(toolbar)는 자주 사용하는 명령들을 더 편리하게 사용할 수 있도록 해줍니다.
 """
 
 import sys
@@ -18,25 +17,23 @@ class MyApp(QMainWindow):
         self.initUI()
 
     def initUI(self):
+        """
+        메뉴바의 경우와 마찬가지로 QAction 객체를 하나 생성합니다.
+        이 객체는 아이콘 (exit.png), 라벨 ('Exit')을 포함하고, 단축키 (Ctrl+Q)를 통해 실행 가능합니다.
+        상태바에 메세지 ('Exit application')를 보여주고, 클릭 시 생성되는 시그널은 quit() 메서드에 연결되어 있습니다.
+
+        """
         exitAction = QAction(QIcon('exit.png'), 'Exit', self)
         exitAction.setShortcut('Ctrl+Q')            # 단축키
         exitAction.setStatusTip('Exit application') # 상태바의 상태팁
         exitAction.triggered.connect(qApp.quit) 
-
+        
         self.statusBar()
 
-        menubar = self.menuBar()
-        menubar.setNativeMenuBar(False)
-        filemenu = menubar.addMenu('&File')
-        filemenu.addAction(exitAction)
+        self.toolbar = self.addToolBar('Exit')
+        self.toolbar.addAction(exitAction)
 
-        """
-        menuBar() 메서드는 메뉴바를 생성합니다. 이어서 'File' 메뉴를 하나 만들고, 거기에 'exitAction' 동작을 추가합니다.
-        '&File'의 앰퍼샌드 (ampersand, &)는 간편하게 단축키를 설정하도록 해줍니다.
-        'F' 앞에 앰퍼샌드가 있으므로 'Alt+F'가 File 메뉴의 단축키가 됩니다. 만약 'i'의 앞에 앰퍼샌드를 넣으면 'Alt+I'가 단축키가 됩니다.
-        """
-
-        self.setWindowTitle('Menubar')
+        self.setWindowTitle('Toolbar')
         self.setGeometry(300, 300, 300, 200)
         self.show()
 
